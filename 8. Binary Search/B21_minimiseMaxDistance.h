4e96365013859a7d58577d744c2eb8ceb8f2a70f
Pragyan <pragyanpatidar14@gmail.com>
2025-01-10 23:58:13 +0530

    Binary search

---
+++ 8. Binary Search/B21_minimiseMaxDistance.h
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#ifndef B21_MINIMISEMAXDISTANCE_H
+#define B21_MINIMISEMAXDISTANCE_H
+
+// https://www.naukri.com/code360/problems/minimise-max-distance_7541449
+
+class B12 {
+public:
+    int numberOfGasStationsRequired(long double dist, vector<int> &arr) {
+        int n = arr.size();
+        int cnt = 0;
+        for (int i = 1; i < n; i++) {
+            int numberInBetween = ((arr[i] - arr[i - 1]) / dist);
+            if ((arr[i] - arr[i - 1]) == (dist * numberInBetween)) {
+                numberInBetween--;
+            }
+            cnt += numberInBetween;
+        }
+        return cnt;
+    }
+
+    double minimiseMaxDistance(vector<int> &arr, int k){
+        int n = arr.size();
+        long double low = 0;
+        long double high = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            high = max(high, (long double)(arr[i + 1] - arr[i]));
+        }
+
+        long double diff = 1e-6 ;
+        while (high - low > diff) {
+            long double mid = (low + high) / (2.0);
+            int cnt = numberOfGasStationsRequired(mid, arr);
+            if (cnt > k) {
+                low = mid;
+            }
+            else {
+                high = mid;
+            }
+        }
+        return high;
+    }
+};
+
+#endif //B21_MINIMISEMAXDISTANCE_H
---
+++ 8. Binary Search/B22_findMedianSortedArrays.h
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#ifndef B22_FINDMEDIANSORTEDARRAYS_H
+#define B22_FINDMEDIANSORTEDARRAYS_H
+
+// https://leetcode.com/problems/median-of-two-sorted-arrays/
+
+class B22 {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+
+    }
+};
+
+#endif //B22_FINDMEDIANSORTEDARRAYS_H
